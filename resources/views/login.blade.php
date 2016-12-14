3d5712c4a8f65a2f56a10a9ea339f8412abbe12b
Sonny Lo <sonnylo@hotmail.com>
2016-12-14 17:43:11 +0100

    html form added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,4 +7,13 @@
 
 @section('content')
         <h1>Login</h1>
+        <?php
+        Form::open(['url' => 'foo/bar']);
+            echo Form::label('email', 'E-Mail Address: ');
+            echo Form::text('email','Example@hotmail.com');
+                echo '<br/>';
+            echo Form::label('username','Username: ');
+            echo Form::text('username','Username');
+         Form::close()
+    ?>
     @endsection
